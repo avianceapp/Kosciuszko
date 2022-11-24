6daaebe67a903484ebd523e74c77121640a35f3f
Defy <61808223+De-y@users.noreply.github.com>
2022-11-23 17:53:53 -0800

    Update login.py

--- routes/login.py
+++ routes/login.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, render_template, redirect
 from prisma.models import User
 from flask_login import login_user, logout_user, login_required, current_user
 from libraries.db.models import UserModel, get_user
+import hashlib
 
 login_blueprint = Blueprint('login', __name__ , template_folder='../pages/', static_folder='../assets/')
 
@@ -14,12 +15,15 @@ def list_create():
 
   if request.method == 'POST':
     data = request.form
-
+    dk = hashlib.sha256(data['password'].encode('utf-8')).hexdigest()
+    data = {'username': data['username'], 'password': dk}
     if data is None:
       return render_template('login.html')
-
-    user = get_user(data['email'])
+    user = get_user(data['username'])
     if user is None:
+      return render_template('login.html')
+    if user.password != dk:
+      print('error')
       return render_template('login.html')
     login_user(UserModel(user))
     return redirect('/dashboard')
