bf8d79f055b2e872083bfd659bcd25e2ff598c05
Defy <61808223+De-y@users.noreply.github.com>
2022-11-23 17:54:19 -0800

    Update register.py

--- routes/register.py
+++ routes/register.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template
 from prisma.models import User
+import hashlib
 
 user_blueprint = Blueprint('register', __name__ , template_folder='../pages/', static_folder='../assets/')
 
@@ -7,19 +8,22 @@ user_blueprint = Blueprint('register', __name__ , template_folder='../pages/', s
 def list_create():
   if request.method == 'POST':
     data = request.form
-
+    print(data)
     if data is None:
       return
 
     name = data.get('username')
     email = data.get('email')
     password = data.get('password')
+    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
     if name is None or email is None:
       return {"error": "You need to provide name and email"}
-    check = User.prisma().find_first(where={'email':email})
-    if check.username == name:
-      return render_template('register.html')
-    user = User.prisma().create(data={'email': email, 'username': name, 'password': password})
+    try:
+      check = User.prisma().find_first(where={'email':email})
+      if check.email == email:
+        return render_template('register.html')
+    except:
+      user = User.prisma().create(data={'email': email, 'username': name, 'password': password})
 
     return dict(user)
   return render_template('register.html')
